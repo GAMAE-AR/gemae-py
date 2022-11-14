43988ea3a0bba913ad9227d70b5c46d11080a57a
Jordi Eguren Brown <jordi@fcaglp.unlp.edu.ar>
2022-11-14 12:10:52 -0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='GAMAE-py',
+      version='0.1',
+      description='GAMEA python scripts',
+      url='https://github.com/GAMAE-AR/gemae-py',
+      author='Jordi Eguren Brown',
+      author_email='jordi.eguren.brown@gmail.com',
+      py_modules=['gemae-py.utils'])
